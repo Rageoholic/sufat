f7777ac66450c849406d543309fb401f686984cc
Hersh Krishna <hershkrishna@protonmail.ch>
2022-12-20 15:14:19 -0600

    Create a surface and impl Drop

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+#![deny(unsafe_op_in_unsafe_fn)]
+
+use std::sync::Arc;
+
 use log::debug;
 use render_context::RenderContext;
 use winit::{
+    dpi::{LogicalSize, Size},
     event::{Event, StartCause, WindowEvent},
     event_loop::EventLoop,
     window::WindowBuilder,
@@ -10,12 +15,15 @@ mod render_context;
 fn main() {
     env_logger::init();
     let event_loop = EventLoop::new();
-    let window = WindowBuilder::new()
-        .with_visible(false)
-        .build(&event_loop)
-        .unwrap();
+    let window = Arc::new(
+        WindowBuilder::new()
+            .with_visible(false)
+            .with_inner_size(Size::Logical(LogicalSize::new(1280f64, 720f64)))
+            .build(&event_loop)
+            .unwrap(),
+    );
 
-    let _render_context = RenderContext::new(&window).unwrap();
+    let render_context = RenderContext::new(window.clone()).unwrap();
 
     event_loop.run(move |event, _target, control_flow| match event {
         Event::NewEvents(StartCause::Init) => {
@@ -31,6 +39,9 @@ fn main() {
             }
         }
         Event::LoopDestroyed => {
+            //temporary capture so we can make sure that render_context is moved
+            //in
+            Some(&render_context);
             debug!("Loop exiting");
         }
         _ => {}
--- src/render_context.rs
+++ src/render_context.rs
@@ -1,18 +1,30 @@
-use std::ffi::CStr;
+use std::{borrow::Cow, ffi::CStr, sync::Arc};
 
 use ash::{
-    extensions::ext::DebugUtils,
-    vk::{self, ApplicationInfo},
+    extensions::{ext::DebugUtils, khr::Surface},
+    vk::{
+        self, ApplicationInfo, DebugUtilsMessageSeverityFlagsEXT,
+        DebugUtilsMessageTypeFlagsEXT, DebugUtilsMessengerCallbackDataEXT,
+        DebugUtilsMessengerCreateInfoEXT, DebugUtilsMessengerEXT, SurfaceKHR,
+    },
     Entry, Instance,
 };
 use cstr::cstr;
-use raw_window_handle::HasRawDisplayHandle;
+use log::Level;
+use raw_window_handle::{HasRawDisplayHandle, HasRawWindowHandle};
 use winit::window::Window;
 
 #[allow(dead_code)]
 pub struct RenderContext {
     entry: Entry,
     instance: Instance,
+    debug_callback: Option<DebugUtilsMessengerEXT>,
+    debug_utils_loader: DebugUtils,
+    surface: SurfaceKHR,
+    surface_callbacks: Surface,
+    //hold on to the window as we need to make sure it is not dropped under any
+    //circumstances until we drop this Arc
+    _window: Arc<Window>,
 }
 
 #[derive(Debug)]
@@ -25,16 +37,21 @@ pub enum RenderContextError {
 }
 
 impl RenderContext {
-    pub fn new(window: &Window) -> Result<RenderContext, RenderContextError> {
+    pub fn new(
+        window: Arc<Window>,
+    ) -> Result<RenderContext, RenderContextError> {
         //SAFETY: Admittedly not actually safe since someone can make a vulkan
         //lib that on startup scribbles all over our memory or some nonsense but
         //this is a game so. Whatever. Risk I can take.
         match unsafe { Entry::load() } {
+            Err(_) => Err(RenderContextError::UnableToLoadLib),
+
             Ok(entry) => {
                 let vk_version = entry
                     .try_enumerate_instance_version()
                     .unwrap()
                     .unwrap_or(vk::make_api_version(0, 1, 0, 0));
+
                 let app_info = ApplicationInfo::builder()
                     .api_version(vk_version)
                     .engine_name(cstr!("Rageware"))
@@ -48,6 +65,7 @@ impl RenderContext {
                     )
                     .unwrap()
                     .to_vec();
+
                 required_extensions.push(DebugUtils::name().as_ptr());
 
                 let ext_props = entry
@@ -143,14 +161,223 @@ impl RenderContext {
                     //SAFETY: we constructed create_instance from a builder
                     //using correct parameters so it should be correct too
                     match unsafe { entry.create_instance(&create_info, None) } {
-                        Ok(instance) => Ok(RenderContext { entry, instance }),
                         Err(_) => {
+                            log::error!(
+                                "We got to creating an instance but it failed\
+                                for some reason"
+                            );
                             Err(RenderContextError::InstanceCreationFailed)
+                        }
+
+                        Ok(instance) => {
+                            log::info!("Successfully created instance");
+
+                            let mut debug_messenger_log_level =
+                                DebugUtilsMessageSeverityFlagsEXT::empty();
+                            let log_level = log::max_level();
+                            if log_level >= Level::Error {
+                                debug_messenger_log_level |=
+                                    DebugUtilsMessageSeverityFlagsEXT::ERROR
+                            }
+                            if log_level >= Level::Warn {
+                                debug_messenger_log_level |=
+                                    DebugUtilsMessageSeverityFlagsEXT::WARNING
+                            }
+                            if log_level >= Level::Info {
+                                debug_messenger_log_level |=
+                                    DebugUtilsMessageSeverityFlagsEXT::INFO
+                            }
+                            if log_level >= Level::Trace {
+                                debug_messenger_log_level |=
+                                    DebugUtilsMessageSeverityFlagsEXT::VERBOSE
+                            }
+
+                            let debug_utils_loader =
+                                DebugUtils::new(&entry, &instance);
+
+                            let debug_info =
+                                DebugUtilsMessengerCreateInfoEXT::builder()
+                                    .message_severity(
+                                        debug_messenger_log_level,
+                                    ).message_type(DebugUtilsMessageTypeFlagsEXT::GENERAL
+                                         | DebugUtilsMessageTypeFlagsEXT::PERFORMANCE |
+                                             DebugUtilsMessageTypeFlagsEXT::VALIDATION)
+                                             .pfn_user_callback(Some(vulkan_debug_callback))
+                                             .build();
+
+                            let debug_callback = unsafe {
+                                debug_utils_loader
+                                    .create_debug_utils_messenger(
+                                        &debug_info,
+                                        None,
+                                    )
+                                    .ok()
+                            };
+
+                            test_debug_callback(&debug_utils_loader);
+                            let surface_callbacks =
+                                Surface::new(&entry, &instance);
+                            let surface = unsafe {
+                                ash_window::create_surface(
+                                    &entry,
+                                    &instance,
+                                    window.raw_display_handle(),
+                                    window.raw_window_handle(),
+                                    None,
+                                )
+                            }
+                            .unwrap();
+                            Ok(RenderContext {
+                                entry,
+                                instance,
+                                debug_callback,
+                                surface,
+                                debug_utils_loader,
+                                _window: window,
+                                surface_callbacks,
+                            })
                         }
                     }
                 }
             }
-            Err(_) => Err(RenderContextError::UnableToLoadLib),
+        }
+    }
+}
+unsafe extern "system" fn vulkan_debug_callback(
+    message_severity: vk::DebugUtilsMessageSeverityFlagsEXT,
+    message_type: vk::DebugUtilsMessageTypeFlagsEXT,
+    p_callback_data: *const vk::DebugUtilsMessengerCallbackDataEXT,
+    _user_data: *mut std::os::raw::c_void,
+) -> vk::Bool32 {
+    //pointer guaranteed to be valid by API rules since this isn't exported
+    let callback_data = unsafe { *p_callback_data };
+    let message_id_number = callback_data.message_id_number;
+
+    let message_id_name = if callback_data.p_message_id_name.is_null() {
+        Cow::from("")
+    } else {
+        //SAFETY: strings from vk are null terminated
+        unsafe {
+            CStr::from_ptr(callback_data.p_message_id_name).to_string_lossy()
+        }
+    };
+
+    let message = if callback_data.p_message.is_null() {
+        Cow::from("")
+    } else {
+        //SAFETY: strings from vk are null terminated
+        unsafe { CStr::from_ptr(callback_data.p_message).to_string_lossy() }
+    };
+
+    match message_severity {
+        DebugUtilsMessageSeverityFlagsEXT::ERROR => {
+            log::error!(
+                "vulkan debug utils.\n\
+            \ttype: {:?}\n\
+            \tid_name: {:?}\n\
+            \tid_num: {:?}\n\
+            \tmessage: {:?}",
+                message_type,
+                message_id_name,
+                message_id_number,
+                message
+            )
+        }
+        DebugUtilsMessageSeverityFlagsEXT::WARNING => {
+            log::warn!(
+                "vulkan debug utils.\n\
+                \ttype: {:?}\n\
+                \tid_name: {:?}\n\
+                \tid_num: {:?}\n\
+                \tmessage: {:?}",
+                message_type,
+                message_id_name,
+                message_id_number,
+                message
+            )
+        }
+        DebugUtilsMessageSeverityFlagsEXT::INFO => {
+            log::info!(
+                "vulkan debug utils.\n\
+                \ttype: {:?}\n\
+                \tid_name: {:?}\n\
+                \tid_num: {:?}\n\
+                \tmessage: {:?}",
+                message_type,
+                message_id_name,
+                message_id_number,
+                message
+            )
+        }
+        DebugUtilsMessageSeverityFlagsEXT::VERBOSE => {
+            log::trace!(
+                "vulkan debug utils.\n\
+                \ttype: {:?}\n\
+                \tid_name: {:?}\n\
+                \tid_num: {:?}\n\
+                \tmessage: {:?}",
+                message_type,
+                message_id_name,
+                message_id_number,
+                message
+            )
+        }
+        _ => {
+            unreachable!()
+        }
+    }
+
+    vk::FALSE
+}
+
+fn test_debug_callback(debug_utils_loader: &DebugUtils) {
+    let callback_data = DebugUtilsMessengerCallbackDataEXT::builder()
+        .message(cstr!("test"))
+        .message_id_name(cstr!("testing"))
+        .message_id_number(1)
+        .build();
+
+    //SAFETY: We built callback_data with a builder and valid cstrs
+    unsafe {
+        debug_utils_loader.submit_debug_utils_message(
+            DebugUtilsMessageSeverityFlagsEXT::ERROR,
+            DebugUtilsMessageTypeFlagsEXT::GENERAL,
+            &callback_data,
+        );
+        debug_utils_loader.submit_debug_utils_message(
+            DebugUtilsMessageSeverityFlagsEXT::WARNING,
+            DebugUtilsMessageTypeFlagsEXT::PERFORMANCE,
+            &callback_data,
+        );
+        debug_utils_loader.submit_debug_utils_message(
+            DebugUtilsMessageSeverityFlagsEXT::INFO,
+            DebugUtilsMessageTypeFlagsEXT::VALIDATION,
+            &callback_data,
+        );
+        debug_utils_loader.submit_debug_utils_message(
+            DebugUtilsMessageSeverityFlagsEXT::VERBOSE,
+            DebugUtilsMessageTypeFlagsEXT::GENERAL,
+            &callback_data,
+        );
+    }
+}
+
+impl Drop for RenderContext {
+    fn drop(&mut self) {
+        log::info!("Destroying render context");
+        //SAFETY: We correctly construct this in new
+        self.debug_callback.map(|debug_callback| unsafe {
+            self.debug_utils_loader
+                .destroy_debug_utils_messenger(debug_callback, None)
+        });
+
+        unsafe {
+            self.surface_callbacks.destroy_surface(self.surface, None);
+        }
+
+        //SAFETY: We correctly construct this in new
+        unsafe {
+            self.instance.destroy_instance(None);
         }
     }
 }
